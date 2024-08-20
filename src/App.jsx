828102c6c9fa169c2d38b0322d2bd05133b84e58
Jo <johj703@nate.com>
2024-08-20 19:47:25 +0900

    nav, HeavyComponent, footer 컴포넌트 넣기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import HeavyComponent from "./components/HeavyComponent";
+
 const App = () => {
   return (
     <div>
       <h1>useMemo</h1>
+      <nav
+        style={{
+          backgroundColor: "yellow",
+          marginBottom: "30px",
+        }}
+      >
+        네비게이션 바
+      </nav>
+      <HeavyComponent />
+      <footer
+        style={{
+          backgroundColor: "green",
+          marginTop: "30px",
+        }}
+      >
+        푸터 영역이에요.
+      </footer>
     </div>
   );
 };
