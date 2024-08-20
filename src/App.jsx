57cea58ebdcb28b0de1deeac978fda2b5f6d0ca6
Jo <johj703@nate.com>
2024-08-20 20:31:28 +0900

    useState 대신에 useInput(커스텀 훅)을 선언하고 onChange에는 두 번째 인자로 만든 이름을 넣어주면 된다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import useInput from "./hooks/useInput";
 
 // custom hook을 만들 때 주의사항
 // 1) custom hook의 함수 이름은 use로 시작하는 것이 좋아요(useInput)
 // 2) 파일 이름은 원하는 대로(useInput)
 const App = () => {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const [title, inChangeTitleHandler] = useInput();
+  const [body, inChangeBodyHandler] = useInput();
 
   return (
     <div>
@@ -13,17 +14,13 @@ const App = () => {
         type="text"
         name="title"
         value={title}
-        onChange={(e) => {
-          setTitle(e.target.event);
-        }}
+        onChange={inChangeTitleHandler}
       />
       <input
         type="text"
         name="body"
         value={body}
-        onChange={(e) => {
-          setBody(e.target.event);
-        }}
+        onChange={inChangeBodyHandler}
       />
     </div>
   );
