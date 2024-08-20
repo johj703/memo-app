3f9bd40ea7997d903076292f23834daaa5684518
Jo <johj703@nate.com>
2024-08-20 19:39:04 +0900

    useMemo를 위한 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,9 @@
-import { useState, useCallback } from "react";
-import Box1 from "./components/Box1";
-import Box2 from "./components/Box2";
-import Box3 from "./components/Box3";
-
-function App() {
-  const [count, setCount] = useState(0);
-  console.log("App 컴포넌트가 렌더링 되었습니다.");
-
-  const onPlusButtonClickHandler = () => {
-    setCount(count + 1);
-  };
-
-  const onMinusButtonClickHandler = () => {
-    setCount(count - 1);
-  };
-
-  const initCount = useCallback(() => {
-    console.log(`[COUNT 변경] ${count}에서 0으로 변경되었습니다.`);
-    setCount(0);
-  }, [count]);
-
+const App = () => {
   return (
-    <>
-      <h3>카운트 예제입니다.</h3>
-      <p>현재 카운트 : {count}</p>
-      <button onClick={onPlusButtonClickHandler}>+</button>
-      <button onClick={onMinusButtonClickHandler}>-</button>
-      <div
-        style={{
-          display: "flex",
-          marginTop: "10px",
-        }}
-      >
-        <Box1 initCount={initCount} />
-        <Box2 />
-        <Box3 />
-      </div>
-    </>
+    <div>
+      <h1>useMemo</h1>
+    </div>
   );
-}
+};
 
 export default App;
