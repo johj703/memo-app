c29007f56d256f6b924f1fb0406772b2c1aacee2
Jo <johj703@nate.com>
2024-08-20 19:58:23 +0900

    무거운 작업을 만들어서 렌더링 되는데 시간이 오래 걸리도록 작성

--- src/components/HeavyComponent.jsx
+++ src/components/HeavyComponent.jsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 const HeavyComponent = () => {
   const [value, setValue] = useState(0);
 
+  const heavyWork = () => {
+    for (let i = 0; i < 10000000000; i++) {}
+    return 100;
+  };
+
+  const sampleValue = heavyWork();
+
   return (
     <div>
-      <p>나는 {value}를 가져오는 엄청 무거운 작업을 하는 컴포넌트야!</p>
+      <p>나는 {sampleValue}를 가져오는 엄청 무거운 작업을 하는 컴포넌트야!</p>
       <button
         onClick={() => {
           setValue(value + 1);
