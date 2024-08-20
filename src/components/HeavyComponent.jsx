55d0070105a959f4b4af3c58ec3cbddb1496155f
Jo <johj703@nate.com>
2024-08-20 19:55:14 +0900

    HeavyComponent 생성

--- src/components/HeavyComponent.jsx
+++ src/components/HeavyComponent.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 const HeavyComponent = () => {
-  return <div>HeavyComponent</div>;
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <p>나는 {value}를 가져오는 엄청 무거운 작업을 하는 컴포넌트야!</p>
+      <button
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        누르면 아래 count가 올라가요!
+      </button>
+      <br />
+      {value}
+    </div>
+  );
 };
 
 export default HeavyComponent;
