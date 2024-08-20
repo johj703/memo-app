ab03b5c16803806ea450e786f06a85a3e78c8c07
Jo <johj703@nate.com>
2024-08-20 15:49:26 +0900

    useState를 사용해서 +,- 버튼 생성과 Box1,2,3 컴포넌트를 return 값에 넣기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import Box1 from "./components/Box1";
+import Box2 from "./components/Box2";
+import Box3 from "./components/Box3";
+
 function App() {
+  const [count, setCount] = useState(0);
   console.log("App 컴포넌트가 렌더링 되었습니다.");
 
   return (
     <>
       <h3>카운트 예제입니다.</h3>
+      <p>현재 카운트 : {count}</p>
+      <button>+</button>
+      <button>-</button>
+      <div>
+        <Box1 />
+        <Box2 />
+        <Box3 />
+      </div>
     </>
   );
 }
